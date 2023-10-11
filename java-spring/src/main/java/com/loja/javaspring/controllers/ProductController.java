129e4b97e4f3ca78e56de0a84c158ec81b3240b3
Gabriel Garbes <galvao.gabrielg@gmail.com>
2023-10-11 12:25:30 -0300

    get all

--- java-spring/src/main/java/com/loja/javaspring/controllers/ProductController.java
+++ java-spring/src/main/java/com/loja/javaspring/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.loja.javaspring.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,9 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
 
 }
